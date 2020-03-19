8df590a20489479ede6cf0320aac04a6e5406943
Nick Martinez <nicolas.martinez.244@my.csun.edu>
2020-03-19 18:32:56 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,59 +1,58 @@
-#displays a tic tac toe board, with board spaces passed as an array
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+end # end board
 
-#coverts a user's place on the board to the index integer
-def input_to_index(user_input)
-  index = user_input.to_i
+
+def input_to_index(input)
+  index = input.to_i
   index -= 1
   return index
-end
+end # end input_to_index
 
-#checks the number to see if 1) the position is already taken and 2)it is an actual number
+
 def valid_move?(board, index)
   def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+    if (array[ind] == " " || array[ind] == "" || array[ind] == nil)
       return false
     else
       return true
     end
-  end
+  end # end position_taken?
 
-  def on_board?(num)
-    if num.between?(0, 8) == true
+  def on_board?(ind)
+    if (ind.between?(0,8) == true)
       return true
     else
       return false
     end
-  end
+  end # end on_board?
 
   if (position_taken?(board, index)) == false && (on_board?(index) == true)
     return true
   else
     return false
   end
-end
+end # end valid_move
 
-#Takes a users num and the board, and places the X or O char in that position
-def move(board, index, character = "X")
-  board[index] = character
+
+def move(board, index, token = "X")
+  board[index] = token
   return board
-end
+end # end move
 
-#Asks user for a number, check if it is valid, and if it's not, recursively continues to ask for a number
-def turn (board)
-  puts "Please enter 1-9:"
-  num = gets.chomp
-  index = input_to_index(num)
+
+def turn(board)
+  puts "Please enter 1-9: "
+  input = gets.strip
+  index = input_to_index(input)
   if valid_move?(board, index) == true
     move(board, index)
     display_board(board)
   else
     turn(board)
   end
-end
+end # end turn
